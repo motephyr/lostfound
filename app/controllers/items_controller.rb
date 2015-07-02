7b06a52d573fa5227f82e2267926ac3da41a9f51
motephyr <motephyr@gmail.com>
2015-07-02 19:44:35 +0800

    fix limit.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,8 @@ class ItemsController < ApplicationController
 
   private
   def show_items(params)
-    if params[:q].present? && (params[:q]["name_cont"].present?)
-      @q.result(distinct: true).order('found_time desc')
+    if params[:q].present? && (params[:q]["name_cont"].present? || params[:q]["found_time"].present?)
+      @q.result(distinct: true).order('found_time desc').limit(1000)
     else
       render :search
     end
